2969a0188ee810396d5953faa6cf9bd25248273f
Vincent Leclair <vinleclair@gmail.com>
2019-03-30 17:46:27 -0400

    Change algorithm to substraction

--- roman-numerals/roman.exs
+++ roman-numerals/roman.exs
@@ -1,27 +1,38 @@
 defmodule Roman do
-  @ones %{1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "V", 6 => "VI", 7 => "VII", 8 => "VIII", 9 => "IX", 0 => ""}
-  @tens %{1 => "X", 2 => "XX", 3 => "XXX", 4 => "XL", 5 => "L", 6 => "LX", 7 => "LXX", 8 => "LXXX", 9 => "XC", 0 => ""}
-  @hundreds %{1 => "C", 2 => "CC", 3 => "CCC", 4 => "CD", 5 => "D", 6 => "DC", 7 => "DCC", 8 => "DCCC", 9 => "CM", 0 => ""}
-  @thousands %{1 => "M", 2 => "MM", 3 => "MMM"}
+  @roman_numerals [
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"}
+  ]
 
   @doc """
   Convert the number to a roman number.
   """
   @spec numerals(pos_integer) :: String.t()
   def numerals(number) do
-    number
-    |> Integer.digits() 
-    |> Enum.reverse()
-    |> Enum.with_index()
-    |> Enum.reverse()
-    |> Enum.reduce("", fn {k, v}, acc -> acc <> Map.get(convert(v), k) end)
+    convert(number, "")
   end
 
-  def convert(pos) when pos == 0, do: @ones
-  def convert(pos) when pos == 1, do: @tens
-  def convert(pos) when pos == 2, do: @hundreds
-  def convert(pos) when pos == 3, do: @thousands
+  defp convert(0, result), do: result 
 
+  defp convert(number, result) do
+    {arabic, roman} =
+      Enum.find(@roman_numerals, fn x -> 
+        {arabic, roman} = x
+        number - arabic >= 0 
+      end)
+    convert(number - arabic, result <> roman)
+  end
 end
 
 IO.inspect Roman.numerals(1)
