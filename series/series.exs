723fae2369aed95d5f069c741ec736c170535804
Vincent Leclair <vinleclair@gmail.com>
2019-04-03 18:21:02 -0400

    Refactor into piping

--- series/series.exs
+++ series/series.exs
@@ -5,22 +5,12 @@ defmodule StringSeries do
   return an empty list.
   """
   @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(_s, size) when size <= 0, do: []
+
   def slices(s, size) do
-    if size > String.length(s) or size <= 0 do
-      []
-    else
-      cursor = size - 1 # account for the zero index
-      slice(s, 0, cursor, String.length(s), [])
-    end
+    s
+    |> String.graphemes
+    |> Enum.chunk_every(size, 1, :discard)
+    |> Enum.map(&Enum.join/1)
   end
-
-  defp slice(_s, _pos, cursor, length, substrings) when cursor == length do
-    substrings
-  end
-
-  defp slice(s, pos, cursor, length, substrings) do
-    substrings = substrings ++ [String.slice(s, pos..cursor)]
-    slice(s, pos + 1, cursor + 1, length, substrings)
-  end
-
 end
