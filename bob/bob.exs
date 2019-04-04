4d4086f9075522178a572127e34363ee16089c1c
Vincent Leclair <vinleclair@gmail.com>
2019-04-04 16:20:18 -0400

    Refactor into defp helper functions

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,16 +1,29 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      Regex.match?(~r/([\p{Lu}]{2,})/, input) and String.ends_with?(input, "?") ->
+      question?(input) and uppercase_letters?(input) ->
         "Calm down, I know what I'm doing!"
-      String.ends_with?(input, "?") ->
+
+      question?(input) ->
         "Sure."
-      String.split(input) == [] ->
-        "Fine. Be that way!"
-      Regex.match?(~r/([\p{L}])/, input) and String.upcase(input) == input ->
+
+      uppercase_letters?(input) ->
         "Whoa, chill out!"
+
+      empty?(input) ->
+        "Fine. Be that way!"
+
       true ->
         "Whatever."
     end
   end
+
+  defp question?(string), do: String.ends_with?(string, "?")
+
+  defp uppercase_letters?(string), 
+    do: String.upcase(string) != String.downcase(string) and String.upcase(string) == string 
+
+  defp empty?(string), do: String.trim(string) == ""
+
 end
+
