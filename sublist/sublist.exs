d19b040e6202e9ceea4dcd059f5a9dec38531c52
Vincent Leclair <vinleclair@gmail.com>
2019-04-05 17:48:22 -0400

    Add sublist check

---
+++ sublist/sublist.exs
@@ -0,0 +1,23 @@
+defmodule Sublist do
+  @doc """
+  Returns whether the first list is a sublist or a superlist of the second list
+  and if not whether it is equal or unequal to the second list.
+  """
+  def compare(a, b) do
+    cond do
+      equals?(a, b) -> :equal
+      contains?(b, a) -> :sublist
+      contains?(a, b) -> :superlist
+      true -> :unequal
+    end
+  end
+
+  defp equals?(a,b), do: a === b
+
+  defp contains?(a, b) when length(a) < length(b), do: false
+  defp contains?(a, b) do
+    a
+    |> Enum.take(length(b))
+    |> equals?(b) or contains?(tl(a), b)
+  end
+end
