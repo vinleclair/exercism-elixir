8dab7a5f625e5796f7923939cdb3da90ad1e6078
Vincent Leclair <vinleclair@gmail.com>
2019-04-10 15:10:03 -0400

    Refactor whitespaces and test

--- scrabble-score/scrabble.exs
+++ scrabble-score/scrabble.exs
@@ -1,6 +1,6 @@
 defmodule Scrabble do
   @letter_values %{
-    ["A","E", "I", "O", "U", "L", "N", "R", "S", "T"] => 1,
+    ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"] => 1,
     ["D", "G"] => 2,
     ["B", "C", "M", "P"] => 3,
     ["F", "H", "V", "W", "Y"] => 4,
@@ -14,6 +14,13 @@ defmodule Scrabble do
   """
   @spec score(String.t()) :: non_neg_integer
   def score(word) do
-    
+    word
+    |> String.upcase
+    |> String.graphemes
+    |> Enum.reduce(0, fn letter, acc -> acc + letter_value(letter) end)
+  end
+
+  defp letter_value(letter) do 
+    Enum.find_value(@letter_values, 0, fn {letters, value} -> if letter in letters, do: value end)
   end
 end
