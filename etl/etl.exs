dffa4baf4e44e2dd9c6efd212df6a2d4cec1d8c9
Vincent Leclair <vinleclair@gmail.com>
2019-05-10 16:53:53 -0400

    Add ETL transform

---
+++ etl/etl.exs
@@ -0,0 +1,18 @@
+defmodule ETL do
+  @doc """
+  Transform an index into an inverted index.
+
+  ## Examples
+
+  iex> ETL.transform(%{"a" => ["ABILITY", "AARDVARK"], "b" => ["BALLAST", "BEAUTY"]})
+  %{"ability" => "a", "aardvark" => "a", "ballast" => "b", "beauty" =>"b"}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    Enum.reduce(input, %{}, fn {k, vs}, acc ->
+      Enum.reduce(vs, acc, fn v, acc ->
+        Map.put(acc, String.downcase(v), k)
+      end)
+    end)
+  end
+end
