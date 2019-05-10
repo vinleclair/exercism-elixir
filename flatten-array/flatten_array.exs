89b56c96b3f769d2a999f6f02234e3263d22d34c
Vincent Leclair <vinleclair@gmail.com>
2019-05-10 16:39:23 -0400

    Add array flatten module

---
+++ flatten-array/flatten_array.exs
@@ -0,0 +1,22 @@
+defmodule FlattenArray do
+  @doc """
+    Accept a list and return the list flattened without nil values.
+
+    ## Examples
+
+      iex> FlattenArray.flatten([1, [2], 3, nil])
+      [1,2,3]
+
+      iex> FlattenArray.flatten([nil, nil])
+      []
+
+  """
+
+  @spec flatten(list) :: list
+  def flatten(list), do: do_flatten(list) 
+
+  defp do_flatten([h|t]), do: do_flatten(h) ++ do_flatten(t)
+  defp do_flatten([]), do: []
+  defp do_flatten(nil), do: []
+  defp do_flatten(elem), do: [elem]
+end
