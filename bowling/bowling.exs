5dfc390b8d753272e437b22cb141c033ead63636
Vincent Leclair <vinleclair@gmail.com>
2019-05-10 14:20:19 -0400

    Refactor variables, function, atoms to snake_case

--- bowling/bowling.exs
+++ bowling/bowling.exs
@@ -1,16 +1,16 @@
 defmodule Bowling do
-  defstruct [:rolls, :currentRoll, :firstInFrame] 
+  defstruct [:rolls, :current_roll, :first_in_frame] 
 
-  @lastFrame 9  
-  @amountOfFrames 21
-  @perfectFrameScore 10
+  @last_frame 9  
+  @amount_of_frames 21
+  @perfect_frame_score 10
 
-  @firstRoll 0
-  @perfectGameLastRoll 11
-  @secondToLastRoll 18
-  @lastRoll 19
-  @bonusRoll 20
-  @notRolled nil
+  @first_roll 0
+  @perfect_game_last_roll 11
+  @second_to_last_roll 18
+  @last_roll 19
+  @bonus_roll 20
+  @not_rolled nil
 
   @doc """
     Creates a new game of bowling that can be used to store the results of
@@ -18,9 +18,9 @@ defmodule Bowling do
   """
   @spec start() :: any
   def start do
-    %__MODULE__{rolls: List.duplicate(@notRolled, @amountOfFrames), 
-      currentRoll: @firstRoll, 
-      firstInFrame: true}
+    %__MODULE__{rolls: List.duplicate(@not_rolled, @amount_of_frames), 
+      current_roll: @first_roll, 
+      first_in_frame: true}
   end
 
   @doc """
@@ -32,24 +32,24 @@ defmodule Bowling do
   def roll(_, roll) when roll < 0, do: {:error, "Negative roll is invalid"}
 
   def roll(%__MODULE__{rolls: rolls, 
-    currentRoll: currentRoll, 
-    firstInFrame: firstInFrame}, roll) do
+    current_roll: current_roll, 
+    first_in_frame: first_in_frame}, roll) do
     cond do
-      Enum.at(rolls, @lastRoll) != @notRolled and 
-      (Enum.at(rolls, @secondToLastRoll) + Enum.at(rolls, @lastRoll)) < @perfectFrameScore ->
+      Enum.at(rolls, @last_roll) != @not_rolled and 
+      (Enum.at(rolls, @second_to_last_roll) + Enum.at(rolls, @last_roll)) < @perfect_frame_score ->
         {:error, "Cannot roll after game is over"}
 
-      roll > @perfectFrameScore or 
-      (firstInFrame == false and roll + Enum.at(rolls, currentRoll - 1) > @perfectFrameScore) ->
+      roll > @perfect_frame_score or 
+      (first_in_frame == false and roll + Enum.at(rolls, current_roll - 1) > @perfect_frame_score) ->
         {:error, "Pin count exceeds pins on the lane"}
 
       true ->
         %__MODULE__{
-          rolls: List.replace_at(rolls, currentRoll, roll), 
-          currentRoll: currentRoll + 1,
-          firstInFrame: 
-          (if firstInFrame == false or 
-          (firstInFrame == true and roll == @perfectFrameScore),
+          rolls: List.replace_at(rolls, current_roll, roll), 
+          current_roll: current_roll + 1,
+          first_in_frame: 
+          (if first_in_frame == false or 
+          (first_in_frame == true and roll == @perfect_frame_score),
             do: true, else: false) 
             }
     end
@@ -61,72 +61,72 @@ defmodule Bowling do
   """
   @spec score(any) :: integer | String.t()
   def score(%__MODULE__{rolls: rolls}) do
-      if incompleteGame?(rolls) or bonusRollsMissing?(rolls) do
+      if incomplete_game?(rolls) or bonus_rolls_missing?(rolls) do
         {:error, "Score cannot be taken until the end of the game"}
       else
         do_score(0, rolls, 0, 0)
       end
   end
 
-  defp do_score(score, rolls, currentFrame, firstInFrame) do
+  defp do_score(score, rolls, current_frame, first_in_frame) do
     cond do
-      currentFrame > @lastFrame ->
+      current_frame > @last_frame ->
         score
 
-      strike?(rolls, firstInFrame) ->
-        do_score(score + @perfectFrameScore + nextTwoBallsForStrike(rolls, firstInFrame),
+      strike?(rolls, first_in_frame) ->
+        do_score(score + @perfect_frame_score + next_two_balls_for_strike(rolls, first_in_frame),
           rolls, 
-          currentFrame + 1, 
-          firstInFrame + 1)
+          current_frame + 1, 
+          first_in_frame + 1)
 
-      spare?(rolls, firstInFrame) ->
-        do_score(score + @perfectFrameScore + nextBallForSpare(rolls, firstInFrame), 
+      spare?(rolls, first_in_frame) ->
+        do_score(score + @perfect_frame_score + next_ball_for_spare(rolls, first_in_frame), 
           rolls, 
-          currentFrame + 1, 
-          firstInFrame + 2)
+          current_frame + 1, 
+          first_in_frame + 2)
 
       true ->
-        do_score(score + twoBallsInFrame(rolls, firstInFrame), 
+        do_score(score + two_balls_in_frame(rolls, first_in_frame), 
           rolls, 
-          currentFrame + 1, 
-          firstInFrame + 2)
+          current_frame + 1, 
+          first_in_frame + 2)
     end
   end
 
-  defp incompleteGame?(rolls) do 
-    Enum.at(rolls, @firstRoll) == @notRolled or 
-    Enum.at(rolls, @perfectGameLastRoll) == @notRolled
+  defp incomplete_game?(rolls) do 
+    Enum.at(rolls, @first_roll) == @not_rolled or 
+    Enum.at(rolls, @perfect_game_last_roll) == @not_rolled
   end 
 
-  defp bonusRollsMissing?(rolls) do
-    (Enum.at(rolls, @secondToLastRoll) == @perfectFrameScore 
-      and Enum.at(rolls, @lastRoll) == @notRolled) or
-    (Enum.at(rolls, @lastRoll) == @perfectFrameScore 
-      and Enum.at(rolls, @bonusRoll) == @notRolled) or
-    (Enum.at(rolls, @secondToLastRoll) < @perfectFrameScore and 
-      Enum.at(rolls, @lastRoll) < @perfectFrameScore and 
-      Enum.at(rolls, @secondToLastRoll) + Enum.at(rolls, @lastRoll) == @perfectFrameScore 
-      and Enum.at(rolls, @bonusRoll) == @notRolled)
+  defp bonus_rolls_missing?(rolls) do
+    (Enum.at(rolls, @second_to_last_roll) == @perfect_frame_score 
+      and Enum.at(rolls, @last_roll) == @not_rolled) or
+    (Enum.at(rolls, @last_roll) == @perfect_frame_score 
+      and Enum.at(rolls, @bonus_roll) == @not_rolled) or
+    (Enum.at(rolls, @second_to_last_roll) < @perfect_frame_score and 
+      Enum.at(rolls, @last_roll) < @perfect_frame_score and 
+      Enum.at(rolls, @second_to_last_roll) + Enum.at(rolls, @last_roll) == @perfect_frame_score 
+      and Enum.at(rolls, @bonus_roll) == @not_rolled)
   end
 
-  defp strike?(rolls, firstInframe) do
-    Enum.at(rolls, firstInframe) == @perfectFrameScore
+  defp strike?(rolls, first_in_frame) do
+    Enum.at(rolls, first_in_frame) == @perfect_frame_score
   end
 
-  defp spare?(rolls, firstInFrame) do
-    Enum.at(rolls, firstInFrame) + Enum.at(rolls, firstInFrame + 1) == @perfectFrameScore
+  defp spare?(rolls, first_in_frame) do
+    Enum.at(rolls, first_in_frame) + Enum.at(rolls, first_in_frame + 1) == @perfect_frame_score
   end
 
-  defp nextTwoBallsForStrike(rolls, firstInFrame) do
-    Enum.at(rolls, firstInFrame + 1) + Enum.at(rolls, firstInFrame + 2)
+  defp next_two_balls_for_strike(rolls, first_in_frame) do
+    Enum.at(rolls, first_in_frame + 1) + Enum.at(rolls, first_in_frame + 2)
   end
   
-  defp nextBallForSpare(rolls, firstInFrame) do
-    Enum.at(rolls, firstInFrame + 2)
+  defp next_ball_for_spare(rolls, first_in_frame) do
+    Enum.at(rolls, first_in_frame + 2)
   end
 
-  defp twoBallsInFrame(rolls, firstInFrame) do 
-    Enum.at(rolls, firstInFrame) + Enum.at(rolls, firstInFrame + 1)
+  defp two_balls_in_frame(rolls, first_in_frame) do 
+    Enum.at(rolls, first_in_frame) + Enum.at(rolls, first_in_frame + 1)
   end
 end
 
